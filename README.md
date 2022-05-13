e8bac8cbe414615186b8518412fe08ebcb92c8f6
MuhammadAfshal <muhammad.afshal@codistan.org>
2022-05-13 12:04:18 +0500

    sparating index.css file, and added comments in bill.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CA3
+# first download the files from github
+# second go to the project directory and run the command `npm install` in terminal
+# after installation, run `npm run start or npm start` command in terminal
+# for creating executeable or for packaging, run `npm run dist` command in terminal.
+# we used electron-builder for creating the executable.
+# use the command `npm run dist` for packaging the application to platform
--- src/bill.js
+++ src/bill.js
@@ -1,7 +1,10 @@
 var data;
+// event handler for calculate bill button
 document.getElementById("BillForm").addEventListener("submit", (e)=>{  
     let formElemnts = new Array(...e.target.elements); 
+    e.preventDefault();
 
+    // take the data from from elements and add in data variable
     data =  (formElemnts.map( (e)=>{ 
     if (e.name){ 
         let inputValue = parseFloat(e.value.trim());
@@ -9,26 +12,32 @@ document.getElementById("BillForm").addEventListener("submit", (e)=>{
     })).filter( (e)=>{ 
         if(e){return e} 
     })
-
+    
+    // make the section 2 visible after hiting on calculate button
     document.getElementById("Section2").style.display="block"
-    document.getElementById("Section1").style.display="none"
     console.log(data)
     let values =  data.map(values => {
     return values[1]
     })
     let units = values[0];
     let days = values[1];
+
+    // calculate the bill without vat
     let billWithOutVat = units * (values [4] / 100) + days * (values [3] / 100)
+
+    // calculate the bill with vat
     let billWithVat = billWithOutVat + (billWithOutVat * (13.5 / 100))
-    console.log('check the result ->', billWithOutVat.toFixed(2), billWithVat.toFixed(2))
+
+    // make the calculated data available for displaying in the table
     document.getElementById("NOfUnits").innerText = units;
     document.getElementById("BillPerid").innerText= days;
     document.getElementById("WithoutVAT").innerText= units +"*" +(values [4] / 100) +"+"+ days +"*"+ (values [3] / 100)+"="+ billWithOutVat.toFixed(2);
     document.getElementById("IncludingVAT").innerText= billWithOutVat +"+"+ (billWithOutVat +"*"+ (13.5 / 100))+ "=" +billWithVat.toFixed(2);
-    document.getElementById("TotalBill").innerText = (billWithOutVat+billWithVat).toFixed(2)
-    e.preventDefault();
 });
-document.getElementById("backbtn").addEventListener("click",(e)=>{
-    document.getElementById("Section2").style.display="none"
-    document.getElementById("Section1").style.display="block"
+
+// event handler for reset button
+document.getElementById("resetBtn").addEventListener("click",(e)=>{
+    document.getElementById("Section2").style.display="none";
+    document.getElementById("numofunits").value= '';
+    document.getElementById("numofdays").value= '';
 })
